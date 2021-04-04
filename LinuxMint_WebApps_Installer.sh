2ef6b223228fcb4d35b5a5a6ecabaeb2a65445af
phoenixbyrd <james.gundlach79@gmail.com>
2021-04-04 18:57:34 -0500

    Linux Mint Webapps Installer for Raspberry Pi 4

---
+++ LinuxMint_WebApps_Installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install -fy $(dirname "$0")/xapps-common_2.0.7-1_all.deb $(dirname "$0")/libxapp1_2.0.7-1_armhf.deb $(dirname "$0")/gir1.2-xapp-1.0_2.0.7-1_armhf.deb $(dirname "$0")/libxapp-dev_2.0.7-1_armhf.deb $(dirname "$0")/libxapp1-dbgsym_2.0.7-1_armhf.deb devhelp $(dirname "$0")/webapp-manager_1.1.1_all.deb
+
+
+
+
